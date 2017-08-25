6070a74a517e4fbd910078bdd0c5ce2e8273bea2
Janin J <170429@cognizant.com>
2017-08-25 12:18:09 +0530

    login view

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from './App';
+import Login from './Login';
+import Home from './Home';
+
+describe('App Component', () => {
+
+  const wrapper = shallow(<App />)
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('should render Login Component', () => {
+    expect(wrapper.find(Login).exists()).toBe(true);
+  });
+
+  it('should render Home Component', () => {
+    wrapper.setState({ loggedIn: true });
+    expect(wrapper.find(Home).exists()).toBe(true);
+  });
+
 });
+
